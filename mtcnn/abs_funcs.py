Fixed number of classes, improved output of results, improved stopping computation.

mtcnn/abs_funcs.py
@@ -84,7 +84,7 @@ def adjust_alpha(abs_params, X_test, truths_test, labels_val, model, alpha, add_
             new_scale = max(new_scale, min_scale)
 
             scales.append(new_scale)
-            #print('Scaling factor: ', new_scale)
+            # print('Scaling factor: ', new_scale)
 
             K.set_value(alpha[k], new_scale * alpha_k)
 
@@ -104,7 +104,7 @@ def adjust_alpha(abs_params, X_test, truths_test, labels_val, model, alpha, add_
             abstains.append(0)
             scales.append(1.0)
 
-    #print(trues, falses, abstains)
+    # print(trues, falses, abstains)
 
     print_abs_stats(task_names, task_list, alpha, scales, trues, falses, abstains, max_abs, min_acc)
     write_abs_stats('abs_stats.csv', alpha, accs, abst)
@@ -153,6 +153,7 @@ def write_abs_stats(stats_file, alphas, accs, abst):
     for k in range((alphas.shape[0])):
         abs_file.write("%10.5e," % abst[k])
     abs_file.write("\n")
+    abs_file.close()
 
 
 def modify_labels(numclasses_out, ytrain, ytest, yval=None):
@@ -195,7 +196,6 @@ def modify_labels(numclasses_out, ytrain, ytest, yval=None):
     mask_vec[:, -1] = 1
     i = np.random.choice(range(labels_train.shape[0]))
     sanity_check = mask_vec[i, :] * labels_train[i, :]
-    #print(sanity_check.shape)
     if ytrain.ndim > 1:
         ll = ytrain.shape[1]
     else:
@@ -240,15 +240,11 @@ def abstention_loss(alpha, mask):
         base_pred = (1 - mask) * y_pred + K.epsilon()
         base_true = y_true
         base_cost = K.categorical_crossentropy(base_true, base_pred)
-        #abs_pred = K.mean(mask * y_pred, axis=-1)
-        abs_pred = K.sum(mask * y_pred, axis=-1)
+        abs_pred = y_pred[:, -1]
         # add some small value to prevent NaN when prediction is abstained
         abs_pred = K.clip(abs_pred, 0, 1. - K.epsilon())
 
-        #return ((1. - abs_pred) * base_cost - alpha * K.log(1. - abs_pred))
         return K.mean((1. - abs_pred) * base_cost - alpha * K.log(1. - abs_pred))
-        #return K.mean((1. - abs_pred) * base_cost + alpha * K.log(abs_pred/(1. - abs_pred)))
-        #return K.mean((1. - abs_pred) * base_cost + alpha *  abs_pred)
 
     loss.__name__ = 'abs_crossentropy'
     return loss
@@ -291,3 +287,60 @@ def abstention_acc_metric(nb_classes):
     metric.__name__ = 'abstention_acc'
     return metric
 
+
+def abstention_metric(nb_classes):
+    """ Function to estimate fraction of the samples where the model is abstaining.
+
+    Parameters
+    ----------
+    nb_classes : int or ndarray
+        Integer or numpy array defining indices of the abstention class
+    """
+    def metric(y_true, y_pred):
+        """
+        Parameters
+        ----------
+        y_true : keras tensor
+            True values to predict
+        y_pred : keras tensor
+            Prediction made by the model.
+            It is assumed that this keras tensor includes extra columns to store the abstaining classes.
+        """
+        # total abstention
+        total_abs = K.sum(K.cast(K.equal(K.argmax(y_pred, axis=-1), nb_classes), 'int64'))
+
+        # total predicted in original classes
+        total_pred = K.sum(K.cast(K.equal(K.argmax(y_pred, axis=-1), K.argmax(y_pred, axis=-1)), 'int64'))
+
+        return total_abs / total_pred
+
+    metric.__name__ = 'abstention'
+    return metric
+
+
+def save_results(task, y_true, y_pred, y_prob, abs_class):
+    # print(task, abs_class)
+    comb = (y_true, y_pred, y_prob)
+    all_dat = np.stack(comb, axis=-1)
+    fmt = '%5d', '%5d', '%10.5f'
+    np.savetxt('abs_' + task + '_results.txt', all_dat, fmt=fmt)
+    #mismatches = np.where(y_true != y_pred)
+    mismatches = np.where(y_true != y_pred)[0]
+    #print(mismatches)
+    #print(np.array(y_prob)[(mismatches)])
+    # print(all_dat[(mismatches)])
+
+    # generate the results on the base classes
+    preds = np.array(y_pred)
+    non_abs = np.where(preds < (abs_class - 1))[0]
+    base_true = y_true[non_abs]
+    base_pred = preds[non_abs]
+    base_prob = np.array(y_prob)[non_abs]
+    #base_pred = preds[preds < abs_class - 1]
+    #base_true = y_true[preds < abs_class - 1]
+
+    mismatches = np.where(base_true != base_pred)[0]
+    # print(mismatches)
+    all_mis = np.stack((base_true, base_pred, base_prob), axis = -1)
+    # print(all_mis)
+    np.savetxt('abs_' + task + '_mismatches.txt', all_mis[mismatches], fmt=fmt)

mtcnn/abs_keras_mt_shared_cnn.py
@@ -36,7 +36,7 @@ import pickle
 import argparse
 import os
 from keras.initializers import RandomUniform, lecun_uniform
-from abs_funcs import abstention_loss, abstention_acc_metric
+from abs_funcs import abstention_loss, abstention_acc_metric, abstention_metric
 #from keras.layers.convolutional import Conv1D
 #np.random.seed(1337)
 
@@ -85,6 +85,7 @@ def init_export_network(task_names,
     FC_models = []
     abs_loss = []
     abs_acc = {}
+    abs_metric = {}
     for i in range(len(num_classes)):
         outlayer = Dense(num_classes[i],
                          name=task_names[i],
@@ -95,8 +96,9 @@ def init_export_network(task_names,
         mask_vec[num_classes[i] - 1] = 1.0
         task_loss = abstention_loss(alpha[i], mask_vec)
         task_acc = abstention_acc_metric(num_classes[i] - 1)
+        task_abs = abstention_metric(num_classes[i] - 1)
         abs_loss.append(task_loss)
-        abs_acc.update({outname: task_acc})
+        abs_acc.update({outname: [task_acc, task_abs]})
 
     # the multitask model
     model = Model(inputs=model_input, outputs=FC_models)

mtcnn/abs_mt_cnn_exp.py
@@ -36,7 +36,7 @@ from keras.callbacks import LambdaCallback
 
 from abs_funcs import modify_labels
 from abs_funcs import print_abs_stats, write_abs_stats, adjust_alpha
-from abs_funcs import abstention_loss
+from abs_funcs import abstention_loss, save_results
 
 
 def main():
@@ -74,8 +74,8 @@ def main():
     params = {}
     params.update({'max_abs': [0.1, 0.3]})
     params.update({'min_acc': [0.98, 0.90]})
-    params.update({'abs_gain': 1.0})
-    params.update({'acc_gain': 1.0})
+    params.update({'abs_gain': 0.5})
+    params.update({'acc_gain': 0.5})
     params.update({'alpha_scale_factor': [0.8, 0.8]})
     params.update({'abs_tasks': [1, 1]})
     params.update({'alpha_init': [0.1, 0.1]})
@@ -115,7 +115,8 @@ def main():
     abs_index = []
 
     for i in range(num_tasks):
-        num_classes.append(max_classes[i] + 1 + params['abs_tasks'][i])
+        num_classes.append(max_classes[i] + params['abs_tasks'][i])
+        print(num_classes[i])
         new_train_i, new_test_i, new_val_i = modify_labels(num_classes[i],
                                                            train_y[:, i],
                                                            test_y[:, i],
@@ -160,14 +161,26 @@ def main():
 
     checkpointer = ModelCheckpoint(filepath=model_name, verbose=1, save_best_only=True)
     stopper = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='auto')
+    f = open('abs_stats.csv', 'w+')
+    for k in range(num_tasks):
+        f.write("%s," % ('alpha_' + tasks[k]))
+    for k in range(num_tasks):
+        f.write("%s," % ('acc_' + tasks[k]))
+    for k in range(num_tasks):
+        f.write("%s," % ('abs_' + tasks[k]))
+    f.write("\n")
+    f.close()
 
     # initialize manual early stopping
     min_val_loss = np.Inf
+    min_comb_loss = 0.01
     min_scale_norm = 0.01
     scale_norm = np.Inf
     p_count = 0
     patience = 10
     monitor = 'val_loss'
+    model_saved = False
+    saved_epoch = 0
 
     for epoch in range(epochs):
         h = cnn.fit(x=np.array(train_x),
@@ -176,30 +189,33 @@ def main():
                     initial_epoch=epoch,
                     epochs=epoch + params['n_iters'],
                     verbose=2,
-                    validation_data=validation_data,
-                    callbacks=[checkpointer, stopper])
-
-        # manual early-stopping
-        if (h.history[monitor][0] < min_val_loss) and (scale_norm < min_scale_norm):
-            print("Scaling improved from ", min_scale_norm, " to ", scale_norm)
-            print(monitor, " improved from ", min_val_loss, " to ", h.history[monitor][0])
-            print("Saving model")
-            # set new min, reset patience
-            min_val_loss = h.history[monitor][0]
-            min_scale_norm = scale_norm
-            cnn.save(model_name) # , save_weights_only=False)
-            p_count = 0
-        else:
-            # increment patience
-            p_count += 1
-
-        if p_count >= patience: # patience limit
-            if scale_norm < min_scale_norm: # scaling factors close to 1 (abstention and accuracy limit satisfied)
-                break
+                    validation_data=validation_data)
 
         scales, alpha = adjust_alpha(params, val_x, val_y, val_labels, cnn, alpha, abs_index)
-        scale_norm = np.linalg.norm(np.array(scales) - 1.0, ord=1)
-        print(scale_norm, h.history[monitor][0] * scale_norm)
+        scale_norm = np.linalg.norm(np.array(scales) - 1.0, ord=2)
+
+        # manual early-stopping
+        combined_loss = h.history[monitor][0] * scale_norm
+        if combined_loss < min_comb_loss:
+            print("Combined loss improved from ", min_comb_loss, " to ", combined_loss)
+            print("Scaling is ", scale_norm)
+            print(monitor, " is: ", h.history[monitor][0])
+            print("Saving model")
+            # set new min, reset patience
+            min_comb_loss = combined_loss
+            cnn.save(model_name) # , save_weights_only=False)
+            p_count = 0
+            saved_epoch = epoch
+            model_saved = True
+        elif model_saved:
+            # increment patience
+            print('Stopping criterion did not improve from %.4f at epoch %d' % (min_comb_loss, saved_epoch))
+            p_count += 1
+
+            if p_count >= patience: # patience limit
+                break
+        else:
+            print('Stopping criterion not yet satisfied %.4f > %.4f' % (combined_loss, min_comb_loss))
 
     # Predict on Test data
     trues = []
@@ -209,7 +225,11 @@ def main():
     task_list = params['task_list']
     max_abs = params['max_abs']
     min_acc = params['min_acc']
-    cnn.load_weights(model_name)
+    if model_saved:  # otherwise use latest
+        print("Loading best saved model from epoch", saved_epoch)
+        cnn.load_weights(model_name)
+    else:
+        print("Using latest model")
     pred_probs = cnn.predict(np.array(test_x))
     print('Prediction on test set')
     for t in range(len(tasks)):
@@ -227,13 +247,16 @@ def main():
         falses.append(false)
         abstains.append(abstain)
 
+        # generate the results on the base classes
         preds = np.array(y_pred)
         base_pred = preds[preds < num_classes[t] - 1]
         base_true = y_true[preds < num_classes[t] - 1]
 
         micro = f1_score(base_true, base_pred, average='micro')
         macro = f1_score(base_true, base_pred, average='macro')
-        print('task %s test f-score: %.4f, %.4f' % (tasks[t], micro, macro))
+        print('task %12s test f-score: %.4f, %.4f, abstention: %.4f ' % (tasks[t], micro, macro, abstain / (true + false)))
+        #print(confusion_matrix(base_true, base_pred))
+        save_results(tasks[t], y_true, y_pred, y_prob, num_classes[t])
 
     print("Detailed abstention results")
     print_abs_stats(tasks, task_list, alpha, scales, trues, falses, abstains, max_abs, min_acc)